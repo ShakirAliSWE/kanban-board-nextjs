d85f187834f635d905e485d22270feace5be32f5
Shakir Ali <shakiraliswe@gmail.com>
2024-08-13 11:54:32 +0500

    toggle-button-added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,45 @@
+"use client";
+import { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import KanbanBoard from "@/components/KanbanBoard/KanbanBoard";
+import CustomToggleButtton from "@/components/Form/CustomToggleButtton";
+
+import { CiCalendar } from "react-icons/ci";
+import { MdOutlineViewKanban } from "react-icons/md";
+
+const DEFAULT_OPTIONS = [
+  {
+    label: "Kanban",
+    value: "kanban",
+    icon: <MdOutlineViewKanban size={18} />,
+  },
+  {
+    label: "Calender",
+    value: "calender",
+    icon: <CiCalendar size={18} />,
+  },
+];
 
 const HomePage = () => {
+  const [selectedToggle, setSelectedToggle] = useState(DEFAULT_OPTIONS[0].value);
+
+  const handlerToggle = (event) => {
+    setSelectedToggle(event.target.value);
+  };
+
   return (
     <Box display={"flex"} flexDirection={"column"}>
-      <Box display={"flex"} justifyContent={"center"} mb={2} mt={2}>
+      <Box display={"flex"} justifyContent={"center"} mt={1}>
         <Typography variant="h5">Kanban Board Example</Typography>
       </Box>
-      <KanbanBoard />
+      <Box mb={2}>
+        <CustomToggleButtton
+          options={DEFAULT_OPTIONS}
+          value={selectedToggle}
+          onChange={handlerToggle}
+        />
+      </Box>
+      {selectedToggle === "kanban" && <KanbanBoard />}
     </Box>
   );
 };
